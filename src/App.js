a9fe62af8179abdd6a197b75070e0cd0d45a9770
Mario-Hernandez-S <100177789+Mario-Hernandez-S@users.noreply.github.com>
2022-05-25 23:11:50 -0500

    Actividad 10
    Actividad lista_clientes

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+import {BrowserRouter, Switch, Route} from 'react-router-dom' 
+import getClientes from './lista_clientes';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+     <Switch>
+      <Route path="/" component={getClientes} exact />
+     </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
+/*function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;*/
